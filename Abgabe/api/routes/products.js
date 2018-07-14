e42fc3b5b10b022187207fce8d7ce54ff8ad25c2
Dustin Barth <32930891+dubarth@users.noreply.github.com>
2018-07-14 12:38:39 +0200

    Add files via upload

--- Abgabe/api/routes/products.js
+++ Abgabe/api/routes/products.js
@@ -1,48 +1,126 @@
 const express = require('express');
 const router = express.Router();
+const Product = require('../models/product');
+const mongoose = require('mongoose');
 
-router.get('/', (req,res,next) =>{
-    res.status(200).json({
-        message : 'Handling GET requests to /products'
-    });
+
+// gibt aus wie  viele personen sich im studio befinden
+router.get('/', (req,res,next) => {
+    Product.find()
+        .select('name price _id')
+        .exec().then(docs => {
+        const response = {
+            count: docs.length,
+            products: docs.map(doc =>{
+                return{
+                    name: doc.anem,
+                    price: doc.price,
+                    _id: doc._id,
+                    request:{
+                        type: 'GET',
+                        url:'http://localhost:3005/products/'+doc._id
+                    }
+
+                }
+            })
+        };
+            res.status(200).json(response);
+
+
+    })
+
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 
+// ein nutzer betritt das studio
 router.post('/',(req,res,next) => {
-    const product = {
-        name:req.body.name,
-        price:req.body.price
-    }
-    res.status(201).json({
-        message : 'Handling POST requests to /products',
-        createdProduct: product
+    const product = new Product ({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price
     });
+
+    product.save()
+        .then(result => {
+        console.log(result);
+        res.status(201).json({
+            message : 'Handling POST requests to /products',
+            createdProduct: result
+        });
+
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    })
 });
 
-router.get('/:productID',(req,res,next)=>{
-    const id = req.params.productID;
-    if( id == 'special '){
-        res.status(200).json({
-            message: 'Sie haben die Spezial ID entdeckt'
+//Bekommen die Daten von einem benutzer im Studio mit der MitgliedNr und im welchen bereich er sich findet
+router.get('/:productId',(req,res,next)=> {
+    const id = req.params.productId;
+    Product.findById(id)
+        .exec()
+        .then(doc => {
+                console.log("From database", doc);
+
+                if(doc) {
+                    res.status(200).json(doc);
+                }else{
+                    res.status(404).json({message: 'keine gültige ID gefunden'});
+                }
+                }
+
+        ).catch(err => {
+        console.log(err);
+        res.status(500).json({error: err});
+
+    });
+
+});
+
+    router.patch('/:productId', (req, res, next) => {
+        const id = req.params.productId;
+        const updateOps = {};
+        for (const ops of req.body){
+            updateOps[ops.propName]= ops.value;
+        }
+        //ändert den bereich welcher der Nutzer betritt
+        Product.update({_id: id },{$set: updateOps})
+            .exec()
+            .then(result => {
+                console.log(result);
+                res.stautus(200).json(result);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                    error:err
+                });
+            });
+
+    });
+
+    // Nutzer verlässt das studio
+    router.delete('/:productId', (req, res, next) => {
+        const id = req.params.productId;
+        Product.remove({_id: id }).exec().then(result => {
+            res.status(200).json(result);
         })
-    }else {
-        res.status(200).json({
-            message:'Sie haben eine ID übergeben'
-        })
-    }
-})
-
-router.patch('/:productID',(req,res,next)=>{
-    res.status(200).json({
-        message:'Update produkt!'
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                    error: err
+                });
+            });
     });
-});
-
-router.delete('/:productID',(req,res,next)=>{
-    res.status(200).json({
-        message:'Deleted produkt!'
-    });
-});
 
 
 
